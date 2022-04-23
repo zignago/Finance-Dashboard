cd42d8aec919e78bfb0fa7ea519d8f4b253407ad
Gian Zignago <grzmc7@umsystem.edu>
2022-04-23 15:57:41 -0500

    made graph display within homepage

--- graph_tutorial/tutorial/views.py
+++ graph_tutorial/tutorial/views.py
@@ -13,27 +13,20 @@ def index(request):
 
     df = df.reset_index()
 
+    #converting data into float64 to be plotted
     for i in ['Open', 'High', 'Close', 'Low']:
         df[i] = df[i].astype('float64')    
 
-    '''#data to be plotted
-    x_data = [0,1,2,3,4,8] #for yfinance - date
-    y_data = [9,2,3,0,11,8] #for yfinance - price
-
     #plotting the data
-    plot_div = plot([Scatter(x=x_data, y=y_data,
-                        mode='lines', name='test',
-                        opacity=0.8, marker_color='green')],
-               output_type='div')'''
-    
-    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+    plot_div1 = go.Figure(data=[go.Candlestick(x=df['Date'],
                 open=df['Open'],
                 high=df['High'],
                 low=df['Low'],
-                close=df['Close'])])
+                close=df['Close'])],)
 
-    fig.show()
-    
-    #rendering data
-    #return render(request, "tutorial/index.html", context={'plot_div': plot_div})
-    return render(request, "tutorial/index.html", context={'fig': fig})
+    # this line converts the go.figure() object into a plot() object so it can be
+    # output as a div and natively displayed in the webpage without a new tab
+    # needing to be opened
+    plot_div = plot(plot_div1, output_type='div')
+
+    return render(request, "tutorial/index.html", context={'plot_div': plot_div})
