4b117a75bcec7c5235102af62214386558551781
Gian Zignago <grzmc7@umsystem.edu>
2022-04-24 01:05:02 -0500

    added graph formatting

--- graph_tutorial/tutorial/views.py
+++ graph_tutorial/tutorial/views.py
@@ -4,11 +4,15 @@ from plotly.graph_objs import Scatter
 
 import yfinance as yf
 from datetime import date
-from dateutil.relativedelta import relativedelta
 import plotly.graph_objects as go
 
-#def index(request):
-#    return render(request, 'tutorial/index0.html')
+# TODO:
+# - Verify ticker symbol at page load
+# - Un-hardcode start date for data
+# - Add more info about companies from yfinance
+# - Add animations on page load
+# - Allow users to define date range for price graph
+# - Integrate Database
 
 def index(request):
     if request.method == 'POST':
@@ -20,11 +24,13 @@ def index(request):
             # Gathering data on given symbol from yfinance
             ticker = yf.Ticker(symbol)
 
+            '''
             info = None
             try:
                 info = ticker.info
             except:
-                return HttpResponse("no such ticker")  
+                return HttpResponse("no such ticker")
+            '''
 
             df = ticker.history(period="1y")
 
@@ -35,12 +41,19 @@ def index(request):
             for i in ['Open', 'High', 'Close', 'Low']:
                 df[i] = df[i].astype('float64')    
 
-            # Creates a plot using plotly
+            # Edit the dimensions of the graph
+            layout = go.Layout(
+                #autosize=False,
+                #width=1000,
+                height=1000,)
+
+            # Creates a candlestick graph_object figure using plotly
             plot_div1 = go.Figure(data=[go.Candlestick(x=df['Date'],
                         open=df['Open'],
                         high=df['High'],
                         low=df['Low'],
-                        close=df['Close'])])
+                        close=df['Close'])], 
+                    layout=layout)
 
             # this line converts the go.figure() object into a plot() object so it can be
             # output as a div and natively displayed in the webpage without a new tab
@@ -56,36 +69,3 @@ def index(request):
             return HttpResponse("no such user")  
     else:
         return render(request, 'tutorial/index0.html')
-
-'''
-def index(request):
-
-    # Symbol of the ticker to be accessed and plotted
-    symbol = "PLTR"
-
-    # Gathering data on given symbol from yfinance
-    ticker = yf.Ticker(symbol)
-    df = ticker.history(period="1y")
-
-    # Converts yf data from dictionary into dataframe
-    df = df.reset_index()
-
-    # Converting data into float64 to be plotted
-    for i in ['Open', 'High', 'Close', 'Low']:
-        df[i] = df[i].astype('float64')    
-
-    # Creates a plot using plotly
-    plot_div1 = go.Figure(data=[go.Candlestick(x=df['Date'],
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'])])
-
-    # this line converts the go.figure() object into a plot() object so it can be
-    # output as a div and natively displayed in the webpage without a new tab
-    # needing to be opened
-    plot_div = plot(plot_div1, output_type='div')
-
-    # Sends info to templates/tutorial/index.html
-    return render(request, "tutorial/index.html", context={'plot_div': plot_div, 'symbol': symbol})
-'''
